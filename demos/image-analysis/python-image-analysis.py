0e20f3b7e76051770789fddf8337f93e0e5f28cb
mulasien <matthew-ulasien@pluralsight.com>
2024-03-01 15:32:27 -0600

    Update python-image-analysis.py

--- demos/image-analysis/python-image-analysis.py
+++ demos/image-analysis/python-image-analysis.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from azure.ai.contentsafety import ContentSafetyClient
-from azure.ai.contentsafety.models import AnalyzeImageOptions, ImageData, ImageCategory
+from azure.ai.contentsafety.models import AnalyzeImageOptions, ImageData
 from azure.core.credentials import AzureKeyCredential
 from azure.core.exceptions import HttpResponseError
 
